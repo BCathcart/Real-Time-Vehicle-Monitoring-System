9b61925e0f1951a89ac576c9a7cbaaa7e9493019
Brandon Ma <ma.brandonjh@gmail.com>
2021-12-03 22:17:56 -0500

    added sporatic task

--- src/frequency_adjuster.cpp
+++ src/frequency_adjuster.cpp
@@ -2,7 +2,14 @@
 
 void listen_for_user_commands(TaskTimer* producer_timers[]) {
     // TODO: listen for commands and call AdjustInterval() on the timer accordingly
+    int newPeriod;
 
+	while (1){
+		std::cin >> newPeriod;
+		std::cout << "New Period: " << newPeriod << std::endl;
+
+		producer_timers[0]->AdjustInterval(newPeriod * 1000000);
+	}
     // input = scanf()
 
     // input == "x"
