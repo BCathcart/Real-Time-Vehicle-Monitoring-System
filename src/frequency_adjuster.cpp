5ae3cb3c9b6ff7def43504ac66c88d637664a559
Brandon Ma <ma.brandonjh@gmail.com>
2021-12-04 22:57:43 -0500

    changed sporatic task

--- src/frequency_adjuster.cpp
+++ src/frequency_adjuster.cpp
@@ -1,15 +1,28 @@
 #include "frequency_adjuster.h"
-
+#include <string>
+#include <vector>
 #include <iostream>
 
 void listen_for_user_commands(PeriodicTaskStore* periodic_task_store) {
     // TODO: listen for commands and call AdjustInterval() on the timer accordingly
-    int newPeriod;
+    std::string taskChange;
+    std::string delimiter = ",";
+    std::vector<int> newPeriod;
 	while (1){
-		std::cin >> newPeriod;
-		std::cout << "New Period: " << newPeriod << std::endl;
+		std::cin >> taskChange; //must in the form 'task#','newPeriod'
 
-		periodic_task_store->updatePeriod(0, newPeriod * 1000000);
+		size_t pos = 0;
+		std::string token;
+		while ((pos = taskChange.find(delimiter)) != std::string::npos) {
+		    token = taskChange.substr(0, pos);
+		    newPeriod.push_back(std::stoi(token));
+		    taskChange.erase(0, pos + delimiter.length());
+		}
+		newPeriod.push_back(std::stoi(taskChange));
+
+		std::cout << "Task: " << newPeriod[0] << " New Period: " << newPeriod[1] << std::endl;
+		periodic_task_store->updatePeriod(newPeriod[0], newPeriod[1]);
+		newPeriod.clear();
 	}
     // input = scanf()
 
